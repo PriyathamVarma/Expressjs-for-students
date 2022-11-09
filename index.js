de6c5195d68227461418f89e596f95c842581bef
PriyathamVarma <priyatham002@gmail.com>
2022-11-09 15:01:48 +0000

    new things added

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
 
+// MOngoose dependency
+const mongoose = require('mongoose');
+// connect the db
+const DB = process.env.DATABASE;
+const connectDB = mongoose.connect(DB);
+console.log(connectDB);
+
 const app = express();
 // Run express when this page is rendered/loaded
 
@@ -106,6 +113,124 @@ app.patch('/beast/:id',(req,res)=>{
   });
 
   
+});
+
+
+// Destroy
+app.delete('/beast/:id',(req,res)=>{
+  const _id = parseInt(req.params.id);
+
+  const newData = beasts.filter(item => item.id !== _id);
+
+  fs.writeFile('./Data/beasts.json',JSON.stringify(newData),()=>{
+    res.send(newData);
+  })
+
+});
+
+// Mongo DB CRUD operations
+// Create a Schema
+// {"id":2,"name":"Kraken","heads":"1","mythology":"Greek"}
+const beastsSchema = new mongoose.Schema({
+  name:      String,
+  heads:     Number,
+  mythology: String
+});
+
+// MOdal
+const beastsModal = mongoose.model('Names',beastsSchema);
+
+
+
+// Create data
+app.post('/beasts/create',async(req,res)=>{
+
+  // Create new obj
+/*const newData = new beastsModal({
+  name:"Krakens Motherss",
+  heads:2,
+  mythology:"Greek"
+});*/
+
+const _name = req.body.name;
+const _heads = req.body.heads;
+const _mythology = req.body.mythology;
+
+const newData = new beastsModal({
+  name:_name,
+  heads:_heads,
+  mythology:_mythology
+});
+
+
+//const {name,heads,mythology} = req.body;
+
+
+newData.save();
+
+const allData = await beastsModal.find();
+
+res.send(allData);
+
+});
+
+
+// Read the data
+app.get('/beasts/read',async(req,res)=>{
+
+  const allData = await beastsModal.find();
+
+  res.send(allData);
+
+
+});
+
+// Get data by ID
+app.get('/beasts/read/:id',async(req,res)=>{
+
+  const _id = req.params.id;
+
+  const reqData =await beastsModal.findById(_id);
+
+  res.send(reqData);
+
+});
+
+// Updating the info
+app.patch('/beasts/update/:id', async(req,res)=>{
+
+  const _id = req.params.id;
+
+  const reqData = await beastsModal.findById(_id);
+
+
+  reqData.name =" New name";
+
+  reqData.save();
+
+  const allData = await beastsModal.find();
+  res.send(allData);
+
+
+});
+
+
+app.delete('/beasts/delete/:id',async(req,res)=>{
+
+  const _id = req.params.id;
+
+  try{
+    await beastsModal.deleteOne({id:_id});
+
+
+    // Dont bother
+    const allData = await beastsModal.find();
+    res.send(allData);
+
+  }catch(err){
+    console.log(err);
+  }
+
 });
 
 
